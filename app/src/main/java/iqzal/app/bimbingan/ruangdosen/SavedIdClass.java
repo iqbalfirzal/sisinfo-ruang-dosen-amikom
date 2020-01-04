09baf5c171c56d5c13e365566a2bf3df57e5828d
iqbalfirzal <iqbal.firzal@gmail.com>
2020-01-04 17:25:44 +0700

    initial commit

--- app/src/main/java/iqzal/app/bimbingan/ruangdosen/SavedIdClass.java
+++ app/src/main/java/iqzal/app/bimbingan/ruangdosen/SavedIdClass.java
@@ -5,6 +5,9 @@ import android.app.Application;
 public class SavedIdClass extends Application{
 
     private String id;
+    private String chatMyUsername;
+    private String chatWith;
+    private String chatWithName;
 
     public String getId() {
         return id;
@@ -12,5 +15,29 @@ public class SavedIdClass extends Application{
 
     public void setId(String anId) {
         id = anId;
+    }
+
+    public String getChatMyUsername() {
+        return chatMyUsername;
+    }
+
+    public void setChatMyUsername(String anChatMyUsername) {
+        chatMyUsername = anChatMyUsername;
+    }
+
+    public String getChatWith() {
+        return chatWith;
+    }
+
+    public void setChatWith(String anChatWith) {
+        chatWith = anChatWith;
+    }
+
+    public String getChatWithName() {
+        return chatWithName;
+    }
+
+    public void setChatWithName(String anChatWithName) {
+        chatWithName = anChatWithName;
     }
 }
